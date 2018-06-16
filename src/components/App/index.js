29985f9f36d8d4c48ea91135c81eb29c52b14a00
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-06-16 20:27:29 -0300

    Components, Props, State

---
+++ src/components/App/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Intro from '../Intro';
+import './App.css';
+
+class App extends Component {
+  state = {
+    series: []
+  }
+
+  componentDidMount() {
+    const series = ["Vikings", "Game of Thrones"];
+
+    setTimeout(() => {
+      this.setState({ series: series });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series List</h1>
+        </header>
+        <Intro message="Here you can find all off your most loved series"/>
+        The length of series array { this.state.series.length } 
+      </div>
+    );
+  }
+}
+
+export default App;
