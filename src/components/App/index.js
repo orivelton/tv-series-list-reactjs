5e8be10fe3ea779c04f2dd18dcf242d7a20ed087
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-06-16 21:17:00 -0300

    Add containers

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 import Intro from '../Intro';
+import Series from '../../containers/Series';
 import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    // const series = ["Vikings", "Game of Thrones"];
-
-    // setTimeout(() => {
-    //   this.setState({ series });
-    // }, 2000);
-
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then( response => response.json())
-      .then(json => this.setState({ series: json}))
-  }
-
   render() {
     return (
       <div className="App">
@@ -27,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all off your most loved series"/>
-        The length of series array { this.state.series.length } 
+        <Series />
       </div>
     );
   }
